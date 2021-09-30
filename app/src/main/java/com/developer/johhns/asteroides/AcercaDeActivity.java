8139f571027aac1779712e8303859539489aebc6
juan sanchez <johhns@hotmail.com>
2021-09-30 13:02:38 -0600

    v1.003

---
+++ app/src/main/java/com/developer/johhns/asteroides/AcercaDeActivity.java
@@ -0,0 +1,14 @@
+package com.developer.johhns.asteroides;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcercaDeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
--- app/src/main/java/com/developer/johhns/asteroides/MainActivity.java
+++ app/src/main/java/com/developer/johhns/asteroides/MainActivity.java
@@ -2,7 +2,9 @@ package com.developer.johhns.asteroides;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void mostrarAcerdaDe(View v){
+        startActivity( new Intent( v.getContext() , AcercaDeActivity.class ));
+    }
+
+
 }
