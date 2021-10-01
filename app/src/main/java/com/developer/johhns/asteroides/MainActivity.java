b0ae6fbd8b1134bcf5f9d0851e6589a448e9880a
johhns <johhns@hotmail.com>
2021-10-01 05:04:42 -0600

    v 1.006

--- app/src/main/java/com/developer/johhns/asteroides/MainActivity.java
+++ app/src/main/java/com/developer/johhns/asteroides/MainActivity.java
@@ -1,20 +1,28 @@
 package com.developer.johhns.asteroides;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnAcercaDe, btnSalir ;
+    Toolbar toolbar ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
 
         btnAcercaDe = findViewById(R.id.btnAcercaDe);
         btnAcercaDe.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +40,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int opcionSeleccionada = item.getItemId() ;
+        switch ( opcionSeleccionada ) {
+            case R.id.acercaDe:
+                mostrarAcerdaDe(null);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void mostrarAcerdaDe(View v) {
