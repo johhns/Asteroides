e81b41b0efc957276c18a7564230df24d22e46cf
johhns <johhns@hotmail.com>
2021-10-01 05:07:57 -0600

    v 1.007

--- app/src/main/java/com/developer/johhns/asteroides/MainActivity.java
+++ app/src/main/java/com/developer/johhns/asteroides/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main);
+        getMenuInflater().inflate(R.menu.menu_main,menu);
         return super.onCreateOptionsMenu(menu);
     }
 
